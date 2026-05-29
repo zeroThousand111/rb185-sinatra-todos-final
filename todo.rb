23c7fab99d94e64880f0aa6d84ada0b4dffcfc03
Graham Jarvis <zeroThousand111@proton.me>
2026-05-29 14:57:27 +0100

    create database_persistence.rb file.  Amend session_persistence.rb file.  RB185 lesson 2 assignment 6.

--- todo.rb
+++ todo.rb
@@ -3,13 +3,15 @@ require "sinatra/reloader"
 require "sinatra/content_for"
 require "tilt/erubi"
 
+require_relative "database_persistence.rb"
+
 configure do
   enable :sessions
   set :session_secret, SecureRandom.hex(32)
   set :erb, :escape_html => true
 end
 
-helpers do
+helpers do 
   def list_complete?(list)
     todos_count(list) > 0 && todos_remaining_count(list) == 0
   end
@@ -41,66 +43,6 @@ helpers do
   end
 end
 
-class SessionPersistence
-  def initialize(session)
-    @session = session
-    @session[:lists] ||= []
-  end
-
-  def find_list(id)
-    @session[:lists].find { |l| l[:id] == id }
-  end
-
-  def all_lists
-    @session[:lists]
-  end
-
-  def create_new_list(list_name)
-    id = next_element_id(@session[:lists])
-    @session[:lists] << { id: id, name: list_name, todos: [] }
-  end
-
-  def delete_list(id)
-    @session[:lists].reject! { |list| list[:id] == id }
-  end
-
-  def update_list_name(id, new_name)
-    list = find_list(id)
-    list[:name] = new_name
-  end
-
-  def create_new_todo(list_id, todo_name)
-    list = find_list(list_id)
-    id = next_element_id(list[:todos])
-    list[:todos] << { id: id, name: todo_name, completed: false }
-  end
-
-  def delete_todo_from_list(list_id, todo_id)
-    list = find_list(list_id)
-    list[:todos].reject! { |todo| todo[:id] == todo_id }
-  end
-
-  def update_todo_status(list_id, todo_id, new_status)
-    list = find_list(list_id)
-    todo = list[:todos].find { |t| t[:id] == todo_id }
-    todo[:completed] = new_status
-  end
-
-  def mark_all_todos_as_completed(list_id)
-    list = find_list(list_id)
-    list[:todos].each do |todo|
-      todo[:completed] = true
-    end
-  end
-
-  private
-
-  def next_element_id(elements)
-    max = elements.map { |todo| todo[:id] }.max || 0
-    max + 1
-  end
-end
-
 def load_list(id)
   list = @storage.find_list(id)
   return list if list
@@ -126,7 +68,7 @@ def error_for_todo(name)
 end
 
 before do
-  @storage = SessionPersistence.new(session)
+  @storage = DatabasePersistence.new 
 end
 
 get "/" do
